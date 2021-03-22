350ccf0bdf5db736aa98974e627ef7173a63ebae
Daniel Mac Mini <danielspady@gmail.com>
2021-03-22 09:45:23 -0400

    Initial Commit

---
+++ CryptoPrices/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoPrices
+//
+//  Created by Daniel Spady on 2021-03-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoPrices/CryptoPricesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPricesApp.swift
+//  CryptoPrices
+//
+//  Created by Daniel Spady on 2021-03-22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPricesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
